30ddd810c7be09c54c4dc9eca9c733bf24f44486
Christy Montejo <christymontejo2003@gmail.com>
2026-05-05 21:23:36 +0800

    fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindv4 from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+
   vite: {
     plugins: [tailwindv4()],
   },
+
+  adapter: vercel(),
 });
